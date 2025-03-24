3f06f36cd4b5ac18881fa56ca4bc8687c94e96be
CromJo <tjk851225@naver.com>
2025-03-24 15:40:09 +0900

    스테이지 씬에서 배경 초기세팅 및 클래스 추가.

---
+++ KDTFrameworkFinal/KDT2Framework/Include/Object/BackgroundObject.cpp
@@ -0,0 +1,36 @@
+﻿#include "BackgroundObject.h"
+#include "../Component/SpriteComponent.h"
+
+CBackgroundObject::CBackgroundObject()
+{
+}
+
+CBackgroundObject::CBackgroundObject(const CBackgroundObject& Obj)
+{
+}
+
+CBackgroundObject::CBackgroundObject(CBackgroundObject&& Obj)
+{
+}
+
+CBackgroundObject::~CBackgroundObject()
+{
+}
+
+bool CBackgroundObject::Init()
+{
+    CSceneObject::Init();
+
+    mRoot = CreateComponent<CSpriteComponent>("Background");
+
+    mRoot->SetTexture("Background", TEXT("Texture/Background/Nikke_Background.png"));
+
+    mRoot->SetWorldPos(0.f, 0.f, 0.f);
+    // 기본사이즈의 1.2배 해줌.
+    mRoot->SetWorldScale(1280.f * 1.2f, 640.f * 1.2f, 1.f);
+    mRoot->SetPivot(0.5f, 0.5f);
+    SetRootComponent(mRoot);
+
+    mRoot->SetRenderLayerName("BackGround");
+    return true;
+}
---
+++ KDTFrameworkFinal/KDT2Framework/Include/Object/BackgroundObject.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "SceneObject.h"
+
+class CBackgroundObject :
+    public CSceneObject
+{
+    friend class CScene;
+
+protected:
+    CBackgroundObject();
+    CBackgroundObject(const CBackgroundObject& Obj);
+    CBackgroundObject(CBackgroundObject&& Obj);
+    virtual ~CBackgroundObject();
+
+protected:
+    CSharedPtr<class CSpriteComponent>  mRoot;
+public:
+    virtual bool Init();
+};
+
